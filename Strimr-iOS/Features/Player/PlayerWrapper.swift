9cf6cf04be1e8f7ef19a02f7a1c5ef7c57413042
Philip Davis <bballdavis@hotmail.com>
2026-07-27 21:47:15 -0500

    feat(player): add optional buffering overlay seam

--- Strimr-iOS/Features/Player/PlayerWrapper.swift
+++ Strimr-iOS/Features/Player/PlayerWrapper.swift
@@ -2,19 +2,23 @@ import SwiftUI
 
 struct PlayerWrapper: View {
     let viewModel: PlayerViewModel
+    let showsBufferingOverlay: Bool
     let isPlaybackAuthorized: (PlexItem) -> Bool
 
     init(
         viewModel: PlayerViewModel,
+        showsBufferingOverlay: Bool = true,
         isPlaybackAuthorized: @escaping (PlexItem) -> Bool = { _ in true }
     ) {
         self.viewModel = viewModel
+        self.showsBufferingOverlay = showsBufferingOverlay
         self.isPlaybackAuthorized = isPlaybackAuthorized
     }
 
     var body: some View {
         PlayerView(
             viewModel: viewModel,
+            showsBufferingOverlay: showsBufferingOverlay,
             isPlaybackAuthorized: isPlaybackAuthorized
         )
             .transition(.opacity)
--- Strimr-tvOS/Features/Player/PlayerTVWrapper.swift
+++ Strimr-tvOS/Features/Player/PlayerTVWrapper.swift
@@ -3,15 +3,18 @@ import SwiftUI
 struct PlayerTVWrapper: View {
     let viewModel: PlayerViewModel
     let onExit: () -> Void
+    let showsBufferingOverlay: Bool
     let isPlaybackAuthorized: (PlexItem) -> Bool
 
     init(
         viewModel: PlayerViewModel,
         onExit: @escaping () -> Void,
+        showsBufferingOverlay: Bool = true,
         isPlaybackAuthorized: @escaping (PlexItem) -> Bool = { _ in true }
     ) {
         self.viewModel = viewModel
         self.onExit = onExit
+        self.showsBufferingOverlay = showsBufferingOverlay
         self.isPlaybackAuthorized = isPlaybackAuthorized
     }
 
@@ -19,6 +22,7 @@ struct PlayerTVWrapper: View {
         PlayerTVView(
             viewModel: viewModel,
             onExit: onExit,
+            showsBufferingOverlay: showsBufferingOverlay,
             isPlaybackAuthorized: isPlaybackAuthorized
         )
     }
